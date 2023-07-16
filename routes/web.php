8fcb6067487a4126de23b11f30e6afece803b3e4
kaiismith <ThangDD.BI12-406@st.usth.edu.vn>
2023-07-16 19:55:49 +0700

    Update Login, Register, exercise Router in Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Mockery\Generator\StringManipulation\Pass\Pass;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/post') -> group(function () {
+    Route::get('/index', 'UserController@list')->name('user.list');
+    Route::get('/create', 'UserController@create')->name('user.create');
+    Route::get('/profile/{id?}', 'UserController@profile')->name('user.profile')->where('id', '[0,9]+');
+    Route::get('/update/{id?}', 'UserController@update')->where('id', '[0, 9]+');
+    Route::get('/delete/{id?}', 'UserController@delete')->where('id', '[0, 9]+');
+});
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
