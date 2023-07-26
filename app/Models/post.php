4119ad49342086b0469e4f51825d8d01ebf9f2ff
kaiismith <ThangDD.BI12-406@st.usth.edu.vn>
2023-07-26 20:58:05 +0700

    Database in Laravel

--- app/Models/post.php
+++ app/Models/post.php
@@ -2,17 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    // Other Post model code...
+    use HasFactory;
+
+    protected $var = [
+        'id', 'title', 'description', 'type', 'image', 'user_id',
+    ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 }
-
-
-?>
